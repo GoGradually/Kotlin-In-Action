e7af477d6df88e28713f94a1331d7cc4d33c6ffe
GoGradually <chkun3109@gmail.com>
2026-01-06 22:21:59 +0900

    feat: coroutineScope.kt 파일 추가 및 비동기 합계 계산 구현

---
+++ src/main/kotlin/structuredConcurrency/coroutineScope.kt
@@ -0,0 +1,28 @@
+package structuredConcurrency
+
+import coroutine.log
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.random.Random
+import kotlin.time.Duration.Companion.milliseconds
+
+suspend fun generateValue(): Int{
+    delay(500.milliseconds)
+    return Random.nextInt(0, 10)
+}
+
+suspend fun computeSum() {
+    log("Computing a sum...")
+    val sum = coroutineScope {
+        val a = async { generateValue() }
+        val b = async { generateValue() }
+        a.await() + b.await()
+    }
+    log("The sum is $sum")
+}
+
+fun main() = runBlocking {
+    computeSum()
+}

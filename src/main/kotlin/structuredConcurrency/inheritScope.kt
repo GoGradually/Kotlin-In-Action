0eeddc11f5faa79c2ce91914129ec8df6464581b
GoGradually <chkun3109@gmail.com>
2026-01-06 22:32:03 +0900

    feat: inheritScope.kt 파일 추가 및 코루틴 컨텍스트 로깅 구현

---
+++ src/main/kotlin/structuredConcurrency/inheritScope.kt
@@ -0,0 +1,20 @@
+package structuredConcurrency
+
+import coroutine.log
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+
+fun main() {
+    runBlocking(Dispatchers.Default) {
+        log(coroutineContext)
+        launch {
+            log(coroutineContext)
+            launch(Dispatchers.IO + CoroutineName("mine")) {
+                log(coroutineContext)
+            }
+        }
+    }
+}

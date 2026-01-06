62f9c0ddfb34bcc81353a57cdb49357e1c82f1eb
GoGradually <chkun3109@gmail.com>
2026-01-07 02:11:21 +0900

    feat: flowExample.kt의 지연 시간 단축 및 값 생성 로직 수정

--- src/main/kotlin/flow/flowExample.kt
+++ src/main/kotlin/flow/flowExample.kt
@@ -8,19 +8,16 @@ import kotlinx.coroutines.runBlocking
 fun createValuesFlow(): Flow<Int> {
     return flow {
         emit(1)
-        delay(1000)
+        delay(500)
         emit(2)
-        delay(1000)
-        emit(3)
-        delay(1000)
     }
 }
 
 fun main() = runBlocking {
     val flow = createValuesFlow()
     log("start")
-    delay(2000)
     flow.collect{
         log(it)
+        delay(200)
     }
 }
